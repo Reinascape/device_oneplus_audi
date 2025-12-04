e9acaee04503a46b72b457373e9686030128500f
Reinascape <reinascape@gmail.com>
2025-12-04 05:37:06 +0300

    audi: Adapt to audi
    Change-Id: Ib3c2547d0fa5b56d0353a07e0c9b806ea6848837

--- lineage_audi.mk
+++ lineage_audi.mk
@@ -8,7 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from audi device
-$(call inherit-product, device/oneplus/avalon/device.mk)
+$(call inherit-product, device/oneplus/audi/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
