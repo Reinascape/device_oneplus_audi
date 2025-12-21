e70c2d07c2fcd77fade4c70e8db1dc0b2d734b3c
Reinascape <reinascape@gmail.com>
2025-12-21 14:21:33 +0300

    audi: Set Super size
    Change-Id: I484282cb8f1405f6af0bcccdbc7bdc394beb005e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 #
 
 # Partitions
-BOARD_SUPER_PARTITION_SIZE := 15032385536
+BOARD_SUPER_PARTITION_SIZE := 16106127360
 
 # Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8650-common/BoardConfigCommon.mk
